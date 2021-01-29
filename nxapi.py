0fb165e4cd8f3e366cec28d6dd098ff13e731cba
GregoMiller <opiate1984@gmail.com>
2021-01-29 09:49:56 +0000

    we

--- nxapi.py
+++ nxapi.py
@@ -34,8 +34,8 @@ auth_body = {"aaaUser": {"attributes" : {"name": username, "pwd": password}}}
 
 auth_response = requests.post(auth_url, data=json.dumps(auth_body), timeout=5, verify=False).json()
 token = auth_response['imdata'][0]['aaaLogin']['attributes']['token']
-cookies={}
-cookies['APIC-cookie']=token
+cookies = {}
+cookies['APIC-cookie'] = token
 
 print(token)
 
@@ -65,10 +65,10 @@ while counter < nei_count:
 
     if local_int != 'mgmt0': 
         int_name = str.lower(str(local_int[:3]))
-        int_num = re.search(r'[1-9]/[1-9]*'), local_int)
-        int_url = 'https://192.168.1.37/api/mo/sys/intf/phys-['int_name+str(
+        int_num = re.search(r'[1-9]/[1-9]*', local_int)
+        int_url = 'https://192.168.1.37/api/mo/sys/intf/phys-['(+int_name+str(
             int_num.group(0))+'].json'
-            post_response = requests.post(int_url, data=json.dump(body), headers=myheader, cookies=cookie, verify=False).json()
-            print(post_response)
+        post_response = requests.post(int_url, data=json.dumps(body), headers=myheader, cookies=cookies, verify=False).json()
+        print(post_response)
         
         
